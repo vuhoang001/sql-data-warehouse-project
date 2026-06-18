d2d72dd2d7f0cee6362b82743d9445b833a3516b
hoanggggf <vuhoang001@gmail.com>
2026-06-18 10:34:27 +0700

    feat: init database

---
+++ scripts/init_database.sql
@@ -0,0 +1,30 @@
+USE master; 
+GO
+
+
+IF EXISTS (SELECT name FROM sys.databases WHERE name = N'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+
+CREATE SCHEMA sliver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
+
+
